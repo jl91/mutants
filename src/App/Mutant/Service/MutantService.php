76ad2648e393588b4a175b23db4645f9f7853ca3
JOHN LENNON DE MELO FERNANDES <7700163902@bitbucket.viavarejo.com.br>
2018-12-20 03:53:20 -0200

    adicionando docker compose e afins

--- src/App/Mutant/Service/MutantService.php
+++ src/App/Mutant/Service/MutantService.php
@@ -6,8 +6,20 @@ namespace App\Mutant\Service;
 
 class MutantService
 {
-    public function isMutant(array $dna): bool
-    {
-        return false;
-    }
+    //try {
+//
+//    $mongo = new MongoDB\Driver\Manager('mongodb://root:root@172.20.0.3:27017/magneto');
+//    $query = new MongoDB\Driver\Query([], ['sort' => ['nome' => 1], 'limit' => 5]);
+//
+//    $rows = $mongo->executeQuery("magneto.mutants", $query);
+//
+//    foreach ($rows as $row) {
+//        echo "{$row->nome} : {$row->profissao} <br>";
+//    }
+//} catch (MongoDB\Driver\Exception\Exception $e) {
+//    var_dump($e->getCode());
+//    var_dump($e->getMessage());
+//    var_dump($e->getTraceAsString());
+//}
+
 }
